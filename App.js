aed3a3d7a632d8e81ed3f76abfca24f82383f03c
John M <entity.john@gmail.com>
2024-01-18 14:25:53 -0600

    start with simple functions, looking to add page transition

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  Button,
+} from "react-native";
+import DateTimePicker from "react-native-ui-datepicker";
+import dayjs from "dayjs";
+import { useState } from "react";
 
 export default function App() {
+  const initialTime = dayjs().subtract(18, "year");
+  const [value, setValue] = useState(initialTime);
+  const [isShowResult, setIsShowResult] = useState(false);
+  const resultDate = dayjs(value).add(1_000_000_000, "seconds");
+  const now = dayjs();
+  const isResultDatePast = now.diff(resultDate, "day") > 0;
+  const isResultDateFuture = now.diff(resultDate, "day") < 0;
+  const isResultDateToday = now.diff(resultDate, "day") === 0;
+  const doSomething = () => {
+    se0tIsShowResult(true);
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <Image
+          source={{
+            uri: "https://reactnative.dev/docs/assets/p_cat2.png",
+          }}
+          style={{ width: 200, height: 200 }}
+        />
+        <Text>Enter your birthday below:</Text>
+        <DateTimePicker
+          value={value}
+          onValueChange={(date) => setValue(date)}
+        />
+        <Button
+          title={
+            "Calculate My Billion-Second-Birthday (billionth birthsecond)!"
+          }
+          onPress={doSomething}
+        />
+        <Text>{`Your day is: ${resultDate
+          .format("MMM-DD YYYY HH:mm:ss")
+          .toString()}`}</Text>
+        <StatusBar style="auto" />
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
