e279e95c3499a62dee7c09be8596eb0615b003a3
John M <entity.john@gmail.com>
2024-02-09 01:42:30 -0600

    the date picker i was using has a non-performant time picker

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import React, { Component } from "react";
-import { View, StyleSheet, Dimensions, Text } from "react-native";
+import React, { Component, useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  View,
+  StyleSheet,
+  Dimensions,
+  Text,
+  Image,
+  ScrollView,
+  Button,
+} from "react-native";
+import DateTimePicker from "react-native-ui-datepicker";
 
-// You can import from local files
+import dayjs from "dayjs";
 import Starfield from "./Starfield";
+
 const windowWidth = Dimensions.get("window").width;
-const windowHeight = Dimensions.get("window").height;
-const vw = windowWidth / 100;
 
 export default class App extends Component {
   // const
@@ -17,9 +26,10 @@ export default class App extends Component {
         </View>
         <View style={styles.cardWrapper}>
           <View style={styles.card}>
-            <Text style={styles.text}>What?</Text>
+            <BirthdayForm />
           </View>
         </View>
+        <StatusBar style="auto" />
       </View>
     );
   }
@@ -29,107 +39,74 @@ const styles = StyleSheet.create({
   page: {
     flex: 1,
     position: "relative",
-    // display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    // height: "100vh",
-    // width: "100vw",
     backgroundColor: "#000",
   },
   starfield: {
     position: "absolute",
     top: 0,
     left: 0,
-    // display: "flex",
-    // justifyContent: "center",
-    // alignItems: "center",
     height: "100%",
     width: "100%",
-    backgroundColor: "transparent",
   },
   cardWrapper: {
     position: "absolute",
     top: 0,
     left: 0,
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
     height: "100%",
     width: "100%",
-    backgroundColor: "transparent",
+    justifyContent: "center",
+    alignItems: "center",
   },
   card: {
-    backgroundColor: `hsl(260, 100% 3%)`,
-    color: "rgb(255, 255, 255)",
-    // backgroundColor: `white`,
+    // backgroundColor: `hsl(260, 100% 3%)`,
+    backgroundColor: `white`,
     width: windowWidth / 1.4,
     aspectRatio: 1.5 / 1,
     alignItems: "center",
     justifyContent: "center",
     fontFamily: "sans-serif",
-    borderRadius: 3.6 * vw,
+    borderRadius: 10,
   },
   text: {
     color: "rgb(255, 255, 255)",
   },
 });
 
-// import { StatusBar } from "expo-status-bar";
-// import {
-//   StyleSheet,
-//   Text,
-//   View,
-//   Image,
-//   ScrollView,
-//   Button,
-// } from "react-native";
-// import DateTimePicker from "react-native-ui-datepicker";
-
-// import dayjs from "dayjs";
-// import { useState } from "react";
-
-// export default function App() {
-//   const initialTime = dayjs().subtract(18, "year");
-//   const [value, setValue] = useState(initialTime);
-//   const [isShowResult, setIsShowResult] = useState(false);
-//   const resultDate = dayjs(value).add(1_000_000_000, "seconds");
-//   const now = dayjs();
-//   const isResultDatePast = now.diff(resultDate, "day") > 0;
-//   const isResultDateFuture = now.diff(resultDate, "day") < 0;
-//   const isResultDateToday = now.diff(resultDate, "day") === 0;
-//   const doSomething = () => {
-//     setIsShowResult(true);
-//   };
-//   return (
-//     <ScrollView>
-//       <View style={styles.container}>
-//         <Image
-//           source={{
-//             uri: "https://reactnative.dev/docs/assets/p_cat2.png",
-//           }}
-//           style={{ width: 200, height: 200 }}
-//         />
-//         <Text>Enter your birthday below:</Text>
-//         <DateTimePicker
-//           value={value}
-//           onValueChange={(date) => setValue(date)}
-//         />
-//         <Button title={"Go!"} onPress={doSomething} />
-//         <Text>{`Your day is: ${resultDate
-//           .format("MMM-DD YYYY HH:mm:ss")
-//           .toString()}`}</Text>
-
-//         <StatusBar style="auto" />
-//       </View>
-//     </ScrollView>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-// });
+const BirthdayForm = () => {
+  const initialTime = dayjs().subtract(18, "year");
+  const [value, setValue] = useState(initialTime);
+  const onChange = (event, selectedDate) => {
+    setDate(selectedDate);
+  };
+  const [isShowResult, setIsShowResult] = useState(false);
+  const resultDate = dayjs(value).add(1_000_000_000, "seconds");
+  const now = dayjs();
+  const isResultDatePast = now.diff(resultDate, "day") > 0;
+  const isResultDateFuture = now.diff(resultDate, "day") < 0;
+  const isResultDateToday = now.diff(resultDate, "day") === 0;
+  const doSomething = () => {
+    setIsShowResult(true);
+  };
+  return (
+    <>
+      <Text style={styles.text}>Enter your birthday below:</Text>
+      <DateTimePicker
+        mode="date"
+        headerTextStyle={{ color: "white" }}
+        todayTextStyle={{ color: "white" }}
+        weekDaysTextStyle={{ color: "white" }}
+        // calendarTextStyle={{ color: "white" }}
+        selectedTextStyle={{ color: "white" }}
+        timePickerTextStyle={{ color: "white" }}
+        value={value}
+        onValueChange={(date) => setValue(date)}
+      />
+      <Button title={"Go!"} onPress={doSomething} />
+      <Text style={styles.text}>{`Your day is: ${resultDate
+        .format("MMM-DD YYYY HH:mm:ss")
+        .toString()}`}</Text>
+    </>
+  );
+};
